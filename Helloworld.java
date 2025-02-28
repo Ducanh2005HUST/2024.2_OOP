26edc0787cf4a8cf863b07f9d8dced1826398eec
Ducanh2005HUST <ducanheya@gmail.com>
2025-02-28 16:07:09 +0700

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+//Example 1: HelloWorld.java
+//Text-printing program
+public class Helloworld {
+   public static void main(String arg[]){
+	   System.out.println("Xin chao \n cac ban!");
+	   System.out.println("Hello \t world !");
+   }//end of methods main
+} 
